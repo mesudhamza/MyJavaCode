ad3af3c1be7a71503592f6b67b6d5c2699691b42
Mesud Hasgur <mesudhasgur@Mesuds-MBP.home>
2020-05-04 13:19:13 -0400

    i added all my code

---
+++ src/framework/TestClass.java
@@ -0,0 +1,38 @@
+package framework;
+
+public class TestClass {
+	int a=1;
+	int b=5;
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		String str= "Ahmet";
+		String abc= "Catalina";
+		
+
+	
+	System.out.println(myMethod(5,5));
+		
+		
+		
+	}
+	
+	public static int myMethod(int a, int d) {
+		
+		if(a == d) {
+			return a;
+		}else {
+			return 0;
+		}
+		
+		
+		
+	}
+
+}
